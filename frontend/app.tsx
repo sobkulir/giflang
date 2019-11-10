bbf30849898f2601a19bba708aaeb81990ac506f
Roman Sobkuliak <r.sobkuliak@gmail.com>
2019-11-10 22:37:33 +0100

    Integrate firebase

--- frontend/app.tsx
+++ frontend/app.tsx
@@ -1,11 +1,16 @@
+import * as firebase from 'firebase'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { Layout } from './components/layout'
+import FirebaseConfig from './firebase/config'
 import './global.scss'
 import { configureStore } from './redux/configure-store'
 
 export const storeInstance = configureStore()
+
+// Initialize Firebase
+firebase.initializeApp(FirebaseConfig)
 
 ReactDOM.render(
     <Provider store={storeInstance}>
